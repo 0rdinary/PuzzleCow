f0f77aa8a513cea5d37047f70490a9a413fdd5ec
Unknown <tdj04131@gmail.com>
2018-05-18 10:42:34 +0900

    ifmoon 수정

--- ifmoon.c
+++ ifmoon.c
@@ -1,5 +1,9 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
 int marble_exist[13][13] = { 0 };
 
 typedef struct marble {
@@ -12,36 +16,18 @@ int main(void) {
 	int xtemp, ytemp, i, j;
 	marble now;
 	while (1) {
-		scanf_s("%d %d %d", &now.x, &now.y, &now.color);
+        srand(time(NULL));
+		scanf("%d %d", &now.x, &now.y);
+        now.color = rand() % 9 + 1;
 		if (now.y < 0) {
 			xtemp = 6; ytemp = 12;
 			while (1) {
-				if (now.x == 0) {
-					if (xtemp + now.x > 12 || xtemp + now.x < 0) {
+					if (xtemp + now.x > 12 || xtemp + now.x < 0) {  //if x is out
 						now.x = -now.x;
 					}
-					else if (ytemp == 0) {
+					else if (ytemp + now.y <= 0) {  //if y arrived ceiling
 						marble_exist[0][xtemp] = now.color;
 						break;
-					}
-					else if (ytemp % 2 == 0 && marble_exist[ytemp-2][xtemp] != 0) {
-						if (xtemp == 6 && ytemp == 12) {
-							printf("You lose");
-							exit(1);
-						}
-						else
-							marble_exist[ytemp][xtemp] = now.color;
-						break;
-					}
-					
-					else {
-						xtemp += now.x;
-						ytemp += now.y;
-					}
-				}
-				else {
-					if (xtemp + now.x > 12 || xtemp + now.x < 0) {
-						now.x = -now.x;
 					}
 					else if (marble_exist[ytemp + now.y][xtemp + now.x] != 0) {
 						if (xtemp == 6 && ytemp == 12) {
@@ -52,21 +38,22 @@ int main(void) {
 							marble_exist[ytemp][xtemp] = now.color;
 						break;
 					}
-					else if (ytemp == 0) {
-						marble_exist[0][xtemp] = now.color;
-						break;
-					}
+					
 					else {
+                        marble_exist[ytemp][xtemp] = now.color;
 						xtemp += now.x;
 						ytemp += now.y;
 					}
-				}
 			}
 			for (i = 0; i < 13; i++) {
 				for (j = 0; j < 13; j++)
 					printf("%d ", marble_exist[i][j]);
 				printf("\n");
 			}
+            fflush(0);
 		}
+        else{
+            printf("Y value must be negative\n");
+        }
 	}
 }
